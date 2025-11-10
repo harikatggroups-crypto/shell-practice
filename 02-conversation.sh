4b5faf4c38a8cb252400a6f63455978d5fd452ca
harika dharavath <harika@joindevops.com>
2025-11-10 18:09:00 +0530

    some message

---
+++ 02-conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash/
+person1="harika"
+person2="ravali"
+
+echo "$person1: Hi $person2, how are you?"
+echo "$person2: Hello $person1, I'm good! How about you?"
+echo "$person1: I'm doing well, thanks for asking!"
+echo "$person2: That's great to hear!"
