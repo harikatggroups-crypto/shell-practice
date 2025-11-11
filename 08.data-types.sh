e7d9c02079ddd048c3f6602985b4bf1fa2878af0
harika dharavath <harika@joindevops.com>
2025-11-11 19:14:35 +0530

    some message

--- 08.data-types.sh
+++ 08.data-types.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 #everyhting in shell is considered as string
-NUMBER1=100
+NUMBER1=100 #here 100 is also taken as string or text
 NUMBER2=200
-NAME=HARIKA 
+NAME=HARIKA #here HARIKA is also taken as string or text
+ #arthemetic operation should be done inside double parenthesis
+SUM=$((NUMBER1 + NUMBER2+NAME)) #name is string so it will be considered as 0
+#suppose SUM=$((NUMBER1 + NAME )) then it will be considered as 100+0=100
+#SUM=$((string + number1)) error will be thrown
 
-SUM=$((NUMBER1 + NUMBER2+NAME))
 echo "Sum of $NUMBER1 and $NUMBER2 is: $SUM"
+
+LEADERS="gandhi, mandela, martin luther king jr, jawaharlal nehru"
+#to get number of characters in a string
+echo "Number of characters in LEADERS string is: ${#LEADERS}"
+
+PEOPLE=("gandhi" "mandela" "martin luther king jr" "jawaharlal nehru")
+#to get number of elements in an array
+echo "Number of elements in PEOPLE array is: ${#PEOPLE[@]}"
+
