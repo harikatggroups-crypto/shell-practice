d5eafb18cf1cf9418509475cbbeb822f03a347e4
harika dharavath <harika@joindevops.com>
2025-11-12 16:41:38 +0530

    message

--- 11.optimized-installation.sh
+++ 11.optimized-installation.sh
@@ -6,22 +6,24 @@ if [ $USERID -ne 0 ]; then
    exit 1
 fi
 
-VALIDATE COMMAND() {
+VALIDATECOMMAND(){ #no space should be between validate command and ()
     if [ $1 -ne 0 ]; then
         echo "Error: $2 installation failed."
         #ACCORDING to our present code $2 is mysql ,mgodb,ngnix
         exit 1
+        
+
     else 
         echo "$2 installed successfully."
     fi
-}
+}             
 
 dnf install nginx -y
-VALIDATE COMMAND $? "Nginx" #without calling it will not execute here we called the validate function
+VALIDATECOMMAND $? "Nginx" #without calling it will not execute here we called the validate function
 
 dnf install mongodb-mongosh -y
-VALIDATE COMMAND $? "Mongodb" #without calling it will not execute
+VALIDATECOMMAND $? "Mongodb" #without calling it will not execute
 
 dnf install mysql -y
-VALIDATE COMMAND $? "Mysql"
-# code optimized by using function to reduce repetation 
+VALIDATECOMMAND $? "Mysql"
+ 
