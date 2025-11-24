b093223f21532033c4fc2969b3f1e8afb30256d8
harika dharavath <harika@joindevops.com>
2025-11-24 19:00:17 +0530

    open

--- 17.script1.sh
+++ 17.script1.sh
@@ -3,4 +3,5 @@
 NAME=harika
 echo "my name is $NAME"
 echo " PID of the script is $$"
-sh 18.script2.sh   # iam calling script 2 in  script 1
+#sh 18.script2.sh   # iam calling script 2 in  script 1
+source 18.script2.sh  # source command will bring all the variables and functions of script2 to script1
