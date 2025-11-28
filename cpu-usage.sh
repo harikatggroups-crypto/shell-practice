944454c274b9bca6c89cc6959ea0a2e76ce1a3c4
harika dharavath <harika@joindevops.com>
2025-11-28 21:00:26 +0530

    message

--- cpu-usage.sh
+++ cpu-usage.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
+DISK_THRESHOLD=2 #normally in projects we use 75
 
-while IFS= read -r line  
+while IFS=',' read -r line  #IFS internal field seperator
+#IFS= ',' read -r line #its separated by ,”.  
 do
     echo "$line"
 done <<< "$DISK_USAGE" # "<<<" string	Read input from a string
